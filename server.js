a0ced2f79237543978d5779070a4b1c0deb5ac63
Nader Chehab <naderchehab@gmail.com>
2015-02-25 00:09:06 -0800

    Added basic auth

--- server.js
+++ server.js
@@ -3,12 +3,20 @@ var express = require("express"),
     bodyParser = require('body-parser'),
     errorHandler = require('errorhandler'),
     methodOverride = require('method-override'),
+    utils = require('./utils'),
     hostname = process.env.HOSTNAME || 'localhost',
     port = parseInt(process.env.PORT, 10) || 4567,
     publicDir = process.argv[2] || __dirname + '/public';
 
+// Before any of the relevant routes...
+app.use('/admin', utils.basicAuth('user', 'pass'));
+
 app.get("/", function (req, res) {
-  res.redirect("/index.html");
+    res.redirect("/index.html");
+});
+
+app.get("/admin", function (req, res) {
+  res.send("Login in successful!");
 });
 
 app.use(methodOverride());
